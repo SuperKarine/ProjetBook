e767cf5ecdccf41991f66a369a54a3f0711fb744
Karine <Karine@example.com>
2025-09-12 04:17:28 +0200

    Ajout des fichiers .env pour tests5

--- templates/page/accueil.php
+++ templates/page/accueil.php
@@ -7,7 +7,7 @@
     Featured
   </div>
   <div class="card-body">
-    <h5 class="card-title">Titre2</h5>
+    <h5 class="card-title">Titre3</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
