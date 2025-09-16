f92080562f2b15bed72e251b2f963f03a61d3fe4
Karine <Karine@example.com>
2025-09-17 00:39:02 +0200

    modification du ssh base64

--- templates/page/accueil.php
+++ templates/page/accueil.php
@@ -7,7 +7,7 @@
     Featured
   </div>
   <div class="card-body">
-    <h5 class="card-title">TitreA</h5>
+    <h5 class="card-title">TitreC</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
